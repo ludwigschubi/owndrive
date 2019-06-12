c8cb3955777f4d33680fc87a7bb9bebd87213217
ludwigschubi <ludwigschubi1@gmail.com>
2019-06-12 12:31:53 +0200

    Add thumbnails for picture files

---
+++ src/functional_components/Files/FileIcons.js
@@ -0,0 +1,7 @@
+import blank from "./icons/File.png";
+
+const icons = {
+    "blank": blank
+}
+
+export default icons;
--- src/functional_components/Files/Files.js
+++ src/functional_components/Files/Files.js
@@ -1,16 +1,35 @@
 import React from "react";
 import styles from "./Files.module.css";
+import icons from "./FileIcons";
 
 const Files = props => {
   const files = props.files
     ? props.files.map((file, index) => {
+        if (file.split(".").length > 1) {
+          const imageTypes = ["ico", "png", "jpeg", "jpg"];
+          if (imageTypes.indexOf(file.split(".")[1]) > -1) {
+            return (
+              <li
+                className={styles.file}
+                key={"file" + index}
+                onClick={() => props.onClick(props.currPath + file)}
+              >
+                <div className={styles.iconContainer}>
+                  <img className={styles.thumbnail} src={props.currPath + file}/>
+                  <img className={styles.fileIcon} src={icons.blank} />
+                </div>
+                <p>{file}</p>
+              </li>
+            );
+          }
+        }
         return (
           <li
             className={styles.file}
             key={"file" + index}
             onClick={() => props.onClick(props.currPath + file)}
           >
-            <img className={styles.fileIcon} src="https://www.shareicon.net/download/2015/04/11/21345_document.ico" />
+            <img className={styles.fileIcon} src={icons.blank} />
             <p>{file}</p>
           </li>
         );
