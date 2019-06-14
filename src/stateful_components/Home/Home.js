934fc43cd8bc05999bd15a2263ca0be9873b0d8b
ludwigschubi <ludwigschubi1@gmail.com>
2019-06-14 15:59:16 +0200

    Fix remained merge conflicts

--- src/stateful_components/Home/Home.js
+++ src/stateful_components/Home/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ns from "solid-namespace";
 import rdf from "rdflib";
@@ -21,27 +20,6 @@ class Home extends React.Component {
           file: undefined
         };
   }
-=======
-import React from 'react';
-import ns from 'solid-namespace';
-import rdf from 'rdflib';
-import styles from './Home.module.css';
-import Breadcrumbs from '../../functional_components/Breadcrumbs/Breadcrumbs';
-import Folders from '../../functional_components/Folders/Folders';
-import Files from '../../functional_components/Files/Files';
-import {getBreadcrumbsFromUrl} from '../../util/url';
-
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            breadcrumbs: undefined,
-            currPath: undefined,
-            webId: props.webId,
-            file: undefined,
-        };
-    }
->>>>>>> 4f386c098fd44b7997c53039cba00c9f09cb6e52
 
     sortContainments(urls) {
         const folders = [];
@@ -64,20 +42,6 @@ class Home extends React.Component {
         const store = rdf.graph();
         const fetcher = new rdf.Fetcher(store);
 
-<<<<<<< HEAD
-  loadCurrentFolder(path, newBreadcrumbs) {
-    const currPath = path
-      ? path
-      : "https://" + this.state.webId.split("/")[2] + "/";
-    Promise.resolve(this.loadFolder(currPath, newBreadcrumbs)).then(
-      sortedContainments => {
-        this.setState({
-          folders: sortedContainments[1],
-          files: sortedContainments[0],
-          currPath: currPath,
-          breadcrumbs: newBreadcrumbs,
-          file: undefined
-=======
         return fetcher.load(url).then((response) => {
             const containments = store.each(
                 rdf.sym(url),
@@ -85,7 +49,6 @@ class Home extends React.Component {
                 null
             );
             return this.sortContainments(containments);
->>>>>>> 4f386c098fd44b7997c53039cba00c9f09cb6e52
         });
     }
 
@@ -120,7 +83,7 @@ class Home extends React.Component {
     }
 
     followPath(path, file) {
-        const breadcrumbs = getBreadcrumbsFromUrl(path);
+        const breadcrumbs = path.replace('https://', '').split('/');
         breadcrumbs.shift();
         const newBreadcrumbs = ['/'];
         breadcrumbs.forEach((breadcrumb) => {
@@ -130,7 +93,6 @@ class Home extends React.Component {
         this.loadCurrentFolder(path, newBreadcrumbs);
     }
 
-<<<<<<< HEAD
   uploadFile(e){
     let webId = this.state.webId;
     let currPath = this.state.currPath;
@@ -143,11 +105,6 @@ class Home extends React.Component {
   componentDidMount() {
     this.loadCurrentFolder(this.state.currPath, ["/"]);
   }
-=======
-    componentDidMount() {
-        this.loadCurrentFolder(undefined, ['/']);
-    }
->>>>>>> 4f386c098fd44b7997c53039cba00c9f09cb6e52
 
     render() {
         const fileMarkup = this.state.file ? (
@@ -158,21 +115,6 @@ class Home extends React.Component {
 
         return (
             <div>
-<<<<<<< HEAD
-              <div className={styles.container}>
-                <Folders
-                  folders={this.state.folders}
-                  currPath={this.state.currPath}
-                  onClick={this.followPath.bind(this)}
-                />
-                <Files
-                  files={this.state.files}
-                  currPath={this.state.currPath}
-                  onClick={this.loadFile.bind(this)}
-                />
-              </div>
-              <FileUpload onChange={this.uploadFile.bind(this)}/>
-=======
                 <Breadcrumbs
                     onClick={this.loadCurrentFolder.bind(this)}
                     breadcrumbs={this.state.breadcrumbs}
@@ -196,7 +138,6 @@ class Home extends React.Component {
                         </div>
                     )}
                 </div>
->>>>>>> 4f386c098fd44b7997c53039cba00c9f09cb6e52
             </div>
         );
     }
