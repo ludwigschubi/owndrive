2bc389ffe84d1bbf3e52e7606a8142b18bdc6008
Bejowe <fast.pfeil@googlemail.com>
2019-06-19 23:53:36 +0200

    Fix thumbnail

--- src/functional_components/File/File.js
+++ src/functional_components/File/File.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './File.module.css';
-export default function File({currPath, onClick, image, label, selectedItem}) {
+export default function File({
+    currPath,
+    onClick,
+    image,
+    label,
+    selectedItem,
+}) {
     const imageTypes = ['ico', 'png', 'jpeg', 'jpg'];
     const isImage = imageTypes.indexOf(label.split('.')[1]) > -1;
     if (isImage) {
         return (
-            <div className={styles.container} style={selectedItem ? {opacity: .5} : undefined} onClick={onClick}>
+            <div
+                className={styles.container}
+                style={selectedItem ? {opacity: 0.5} : undefined}
+                onClick={onClick}
+            >
                 <div className={styles.innerContainer}>
                     <div className={styles.iconContainer}>
                         <img
@@ -20,7 +30,11 @@ export default function File({currPath, onClick, image, label, selectedItem}) {
         );
     } else {
         return (
-            <div className={styles.container} style={selectedItem ? {opacity: .5} : undefined} onClick={onClick}>
+            <div
+                className={styles.container}
+                style={selectedItem ? {opacity: 0.5} : undefined}
+                onClick={onClick}
+            >
                 <div className={styles.innerContainer}>
                     <img className={styles.icon} src={image} />
                     <p className={styles.label}>{label}</p>
--- src/functional_components/ItemList/ItemList.js
+++ src/functional_components/ItemList/ItemList.js
@@ -2,23 +2,48 @@ import React from 'react';
 import {Item} from '../Item';
 import styles from './ItemList.module.css';
 import {File} from '../File';
-const ItemList = ({selectedItems, items, image, onItemClick, currPath, isFile = false}) => {
+const ItemList = ({
+    selectedItems,
+    items,
+    image,
+    onItemClick,
+    currPath,
+    isFile = false,
+}) => {
     const itemComponents = items
         ? items.map((item, index) => {
               return isFile ? (
                   <File
-                      selectedItem={selectedItems.includes(currPath + encodeURIComponent(item)) ? true : undefined}
+                      selectedItem={
+                          selectedItems.includes(
+                              currPath + encodeURIComponent(item)
+                          )
+                              ? true
+                              : undefined
+                      }
                       key={item + index}
                       image={image}
-                      onClick={() => onItemClick(currPath + encodeURIComponent(item))}
+                      onClick={() =>
+                          onItemClick(currPath + encodeURIComponent(item))
+                      }
                       label={item}
+                      currPath={currPath}
                   />
               ) : (
                   <Item
-                      selectedItem = {selectedItems.includes(currPath + encodeURIComponent(item) + "/") ? true : undefined}
+                      selectedItem={
+                          selectedItems.includes(
+                              currPath + encodeURIComponent(item) + '/'
+                          )
+                              ? true
+                              : undefined
+                      }
                       key={item + index}
                       image={image}
-                      onClick={() => onItemClick(currPath + encodeURIComponent(item) + '/')}
+                      onClick={() =>
+                          onItemClick(currPath + encodeURIComponent(item) + '/')
+                      }
+                      currPath={currPath}
                       label={item}
                   />
               );
