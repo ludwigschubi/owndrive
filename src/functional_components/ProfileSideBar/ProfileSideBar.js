d35c0f33322e93779d581c83e84bbae732fbf16a
Bejowe <fast.pfeil@googlemail.com>
2019-06-21 00:29:24 +0200

    Profile Picture - add editbutton on hover

--- src/functional_components/ProfileSideBar/ProfileSideBar.js
+++ src/functional_components/ProfileSideBar/ProfileSideBar.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import styles from './ProfileSideBar.module.css';
 import classNames from 'classnames';
-import editIcon from '../../assets/icons/edit.png';
 import closeIcon from '../../assets/icons/close.png';
-import {editProfile} from '../../utils/profileRDF';
 import {KeyValuePair} from '../KeyValuePair';
+import useHover from '../../hooks/useHover';
+import editIcon from '../../assets/icons/edit.png';
+
 export default function ProfileSideBar({isExpanded, toggleSidebar, user}) {
     const [isEditable, setEditable] = useState(false);
+    const [hoverRef, isHovered] = useHover();
     return (
         <div
             className={classNames(styles.container, {
@@ -21,9 +23,14 @@ export default function ProfileSideBar({isExpanded, toggleSidebar, user}) {
             {user.picture ? (
                 <div className={classNames(styles.head, styles.section)}>
                     <div
+                        ref={hoverRef}
                         className={styles.profilePicture}
                         style={{backgroundImage: `url(${user.picture})`}}
-                    />
+                    >
+                        {isHovered ? (
+                            <img src={editIcon} className={styles.editIcon} />
+                        ) : null}
+                    </div>
                     <div>
                         <p className={styles.name}>{user.name}</p>
                         <p className={styles.job}>{user.job}</p>
