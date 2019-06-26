6cf86081f6dd154bb307b1fdeee62c9f128a8396
ludwigschubi <ludwigschubi1@gmail.com>
2019-06-26 15:26:08 +0200

    Fix responiveness of sidebar

--- src/functional_components/ProfileSideBar/ProfileSideBar.js
+++ src/functional_components/ProfileSideBar/ProfileSideBar.js
@@ -18,99 +18,101 @@ export default function ProfileSideBar({
     const [hoverRef, isHovered] = useHover();
     const [value, updateValue] = useState('');
     return (
-        <div
-            className={classNames(styles.container, {
-                [styles.active]: isExpanded,
-            })}
-        >
-            <img
-                className={styles.closeButton}
-                onClick={toggleSidebar}
-                src={closeIcon}
-            />
-            <div className={classNames(styles.head, styles.section)}>
-                {user.picture ? (
-                    <div
-                        ref={hoverRef}
-                        className={styles.profilePicture}
-                        style={{ backgroundImage: `url(${user.picture})` }}
-                    >
-                        <input
-                            type="file"
-                            onChange={onPictureChange}
-                            style={{ display: 'none' }}
-                            id="pictureUpload"
-                            accept="*/*"
-                        />
-                        {isHovered ? (
-                            <label htmlFor="pictureUpload">
-                                <img
-                                    src={editIcon}
-                                    className={styles.editIcon}
-                                />
-                            </label>
-                        ) : (
+        <div className={isExpanded ? styles.background : undefined}>
+            <div
+                className={classNames(styles.container, {
+                    [styles.active]: isExpanded,
+                })}
+            >
+                <img
+                    className={styles.closeButton}
+                    onClick={toggleSidebar}
+                    src={closeIcon}
+                />
+                <div className={classNames(styles.head, styles.section)}>
+                    {user.picture ? (
+                        <div
+                            ref={hoverRef}
+                            className={styles.profilePicture}
+                            style={{ backgroundImage: `url(${user.picture})` }}
+                        >
+                            <input
+                                type="file"
+                                onChange={onPictureChange}
+                                style={{ display: 'none' }}
+                                id="pictureUpload"
+                                accept="*/*"
+                            />
+                            {isHovered ? (
+                                <label htmlFor="pictureUpload">
+                                    <img
+                                        src={editIcon}
+                                        className={styles.editIcon}
+                                    />
+                                </label>
+                            ) : (
+                                <p className={classNames(styles.pictureChange)}>
+                                    Click to Change
+                                </p>
+                            )}
+                        </div>
+                    ) : (
+                        <label
+                            htmlFor="pictureUpload"
+                            ref={hoverRef}
+                            className={styles.label}
+                        >
+                            <img
+                                className={styles.profilePicture}
+                                src={defaultIcon}
+                            />
+                            <input
+                                type="file"
+                                onChange={onPictureChange}
+                                style={{ display: 'none' }}
+                                id="pictureUpload"
+                                accept="*/*"
+                            />
                             <p className={classNames(styles.pictureChange)}>
                                 Click to Change
                             </p>
-                        )}
+                            {isHovered ? (
+                                <label htmlFor="pictureUpload">
+                                    <input
+                                        type="file"
+                                        onChange={onPictureChange}
+                                        style={{ display: 'none' }}
+                                        id="pictureUpload"
+                                        accept="*/*"
+                                    />
+                                    <img
+                                        src={editIcon}
+                                        className={styles.editIcon}
+                                    />
+                                </label>
+                            ) : (
+                                undefined
+                            )}
+                        </label>
+                    )}
+                    <div>
+                        <p className={styles.name}>{user.name}</p>
+                        <p className={styles.job}>{user.job}</p>
                     </div>
-                ) : (
-                    <label
-                        htmlFor="pictureUpload"
-                        ref={hoverRef}
-                        className={styles.label}
-                    >
-                        <img
-                            className={styles.profilePicture}
-                            src={defaultIcon}
-                        />
-                        <input
-                            type="file"
-                            onChange={onPictureChange}
-                            style={{ display: 'none' }}
-                            id="pictureUpload"
-                            accept="*/*"
-                        />
-                        <p className={classNames(styles.pictureChange)}>
-                            Click to Change
-                        </p>
-                        {isHovered ? (
-                            <label htmlFor="pictureUpload">
-                                <input
-                                    type="file"
-                                    onChange={onPictureChange}
-                                    style={{ display: 'none' }}
-                                    id="pictureUpload"
-                                    accept="*/*"
-                                />
-                                <img
-                                    src={editIcon}
-                                    className={styles.editIcon}
-                                />
-                            </label>
-                        ) : (
-                            undefined
-                        )}
-                    </label>
-                )}
-                <div>
-                    <p className={styles.name}>{user.name}</p>
-                    <p className={styles.job}>{user.job}</p>
                 </div>
+                {Object.keys(user).map((key, index) => (
+                    <KeyValuePair
+                        key={key + index}
+                        keyVal={key}
+                        currentValues={value}
+                        values={user[key]}
+                        onUpdate={onProfileUpdate}
+                        onEdit={updateValue}
+                        webId={user['webId']}
+                    />
+                ))}
+                <div className={styles.addButton}>add new category</div>
             </div>
-            {Object.keys(user).map((key, index) => (
-                <KeyValuePair
-                    key={key + index}
-                    keyVal={key}
-                    currentValues={value}
-                    values={user[key]}
-                    onUpdate={onProfileUpdate}
-                    onEdit={updateValue}
-                    webId={user['webId']}
-                />
-            ))}
-            <div className={styles.addButton}>add new category</div>
         </div>
     );
 }
