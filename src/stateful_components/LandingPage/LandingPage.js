f66fb0233cba1abe6e80e3f93e4bf5d6c23afca7
ludwigschubi <ludwigschubi1@gmail.com>
2019-07-14 14:32:39 +0200

    Add Login Component

---
+++ src/stateful_components/LandingPage/LandingPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './LandingPage.module.css';
+
+class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            webId: props.webId,
+        };
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <section className={styles.page}>
+                    <div className={styles.floatAnimation}>
+                        <p className={styles.slogan}>
+                            Because the internet deserves better.
+                        </p>{' '}
+                    </div>
+                </section>
+                <section
+                    className={styles.page}
+                    style={{ backgroundColor: '#333' }}
+                >
+                    <p className={styles.mission}>Owning your digital self.</p>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
--- src/stateful_components/LoginScreen/LoginScreen.js
+++ src/stateful_components/LoginScreen/LoginScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './LoginScreen.module.css';
-import { FlareComponent } from 'flare-react';
-import animation from '../../assets/animations/owntechFloat60fps.flr';
 
 class LoginScreen extends React.Component {
     constructor(props) {
@@ -14,25 +12,7 @@ class LoginScreen extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <section className={styles.page}>
-                    <div className={styles.floatAnimation}>
-                        <FlareComponent
-                            width={500}
-                            height={500}
-                            animationName="Untitled"
-                            file={animation}
-                        />
-                        <p className={styles.slogan}>
-                            Because the internet deserves better.
-                        </p>{' '}
-                    </div>
-                </section>
-                <section
-                    className={styles.page}
-                    style={{ backgroundColor: '#333' }}
-                >
-                    <p className={styles.mission}>Owning your digital self.</p>
-                </section>
+                This is a LoginScreen.
             </div>
         );
     }
